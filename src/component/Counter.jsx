a9f690501624d19e78652d11cf4f035eb96f7d31
Camillo Fallù <camillofallu9920@gmail.com>
2025-02-06 11:22:30 +0100

    esercizio counter completato usando usestate

---
+++ src/component/Counter.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  function incremento() {
+    setCount(count + 1);
+  }
+
+  function decremento() {
+    setCount(count - 1);
+  }
+
+  function reset() {
+    setCount(0);
+  }
+
+  return (
+    <div className="flex justify-center flex-col items-center gap-5 p-5">
+      <p className="font-bold text-5xl text-slate-500">Counter</p>
+      <div className="flex gap-3">
+        <button
+          onClick={incremento}
+          className="p-2 rounded-lg bg-emerald-800 text-amber-50"
+        >
+          Incremento
+        </button>
+        <button
+          onClick={decremento}
+          className="p-2 rounded-lg bg-red-800 text-amber-50"
+        >
+          Decremento
+        </button>
+        <button
+          onClick={reset}
+          className="p-2 rounded-lg bg-blue-600 text-amber-50"
+        >
+          Reset
+        </button>
+      </div>
+      <div className="bg-gray-900 p-2 rounded-full text-amber-50 font-bold text-1xl">
+        <p>{count}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
